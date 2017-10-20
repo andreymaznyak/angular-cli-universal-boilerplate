d4822897927143865554f661490ade2ec5582c9c
Andrey Maznyak <andreymaznyak@gmail.com>
2017-10-20 13:36:45 +1000

    fix scripts for first benchmark

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor( @Inject(PLATFORM_ID) private platformId: Object ) {
+
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit app component is browser:', isPlatformBrowser(this.platformId), 'is server:', isPlatformServer(this.platformId));
+  }
 }
